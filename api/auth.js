e24dd49f180ab7f762db757f2f363d9ba31d552e
Edgardo Cuello <ecuellog@gmail.com>
2020-03-22 23:03:35 -0700

    Add logout functionality

--- api/auth.js
+++ api/auth.js
@@ -4,90 +4,101 @@ var jwt = require('jsonwebtoken');
 var router = express.Router();
 var config = require('../config');
 var User = require('../models/user');
+var RefreshTokenStore = require('../models/refreshTokenStore');
 var tokens = require('../tokens');
 
-//Create new user
+// Create new user
 router.post('/register', (req, res) => {
-	let name = req.body.name;
-	let email = req.body.email;
-    let password = req.body.password;
+  let name = req.body.name;
+  let email = req.body.email;
+  let password = req.body.password;
 
-    if(!name || !email || !password) {
-    	return res.status(400).json({
-    		message: 'Invalid request'
-    	});
+  if(!name || !email || !password) {
+    return res.status(400).json({
+      message: 'Invalid request'
+    });
+  }
+
+  User.findByEmailLogin(email, (err, user) => {
+    if(user){
+      return res.status(400).json({
+        message: 'Email already in use'
+      });
     }
+    let saltRounds = 10;
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      var newUser = new User({
+        name: name,
+        email: email,
+        passwordHash: hash,
+        provider: 'email',
+        subject: 'none'
+      });
 
-	User.findByEmailLogin(email, (err, user) => {
-		if(user){
-			return res.status(400).json({
-            	message: 'Email already in use'
-			});
-		}
-		let saltRounds = 10;
-		bcrypt.hash(password, saltRounds, (err, hash) => {
-			var newUser = new User({
-				name: name,
-				email: email,
-				passwordHash: hash,
-				provider: 'email',
-				subject: 'none'
-			});
-
-			newUser.save(err => {
-				if(err) return next(err);
-				return res.status(200).json({
-                    message: 'Sign up successful',
-                    user: newUser
-                });
-			});
-		});
-	});
+      newUser.save(err => {
+        if(err) return next(err);
+        return res.status(200).json({
+          message: 'Sign up successful',
+          user: newUser
+        });
+      });
+    });
+  });
 });
 
-//Email login only
+// Email login
 router.post('/login', (req, res) => {
-	let email = req.body.email;
-	let password = req.body.password;
+  let email = req.body.email;
+  let password = req.body.password;
 
-	if(!email || !password) {
-		return res.status(400).json({
-			message: 'Invalid request'
-		});
-	}
-	User.findByEmailLogin(email, (err, user) => {
-		if(err) return next(err);
-		if(!user){
-			return res.status(400).json({
-				message: 'Failed to log in. Invalid email or password'
-			});
-		}
-		bcrypt.compare(password, user.passwordHash, (err, valid) => {
-			if(valid){
-				let newTokens = tokens.createNewTokens(user._id, null, (err, newTokens) => {
-					if(err){
-						console.error('Error creating tokens', err);
-						return res.status(500).json({
-							message: 'Server error'
-						})
-					}
-					tokens.setTokenCookies(res, newTokens);
-					return res.status(200).json({
-						user: {
-							_id: user._id,
-							name: user.name,
-							email: user.email
-						},
-						message: 'Login successful'
-					});
-				});
-			} else {
-				return res.status(400).json({
-					message: 'Failed to log in. Invalid email or password.'
-				});
-			}
-		});
-	});
+  if(!email || !password) {
+    return res.status(400).json({
+      message: 'Invalid request'
+    });
+  }
+  User.findByEmailLogin(email, (err, user) => {
+    if(err) return next(err);
+    if(!user){
+      return res.status(400).json({
+        message: 'Failed to log in. Invalid email or password'
+      });
+    }
+    bcrypt.compare(password, user.passwordHash, (err, valid) => {
+      if(valid){
+        tokens.createNewTokens(user._id, null, (err, newTokens) => {
+          if(err){
+            console.error('Error creating tokens', err);
+            return res.status(500).json({
+              message: 'Server error'
+            })
+          }
+          tokens.setTokenCookies(res, newTokens);
+          return res.status(200).json({
+            user: {
+              _id: user._id,
+              name: user.name,
+              email: user.email
+            },
+            message: 'Login successful'
+          });
+        });
+      } else {
+        return res.status(400).json({
+          message: 'Failed to log in. Invalid email or password.'
+        });
+      }
+    });
+  });
+});
+
+router.get('/logout', tokens.checkTokens, (req, res, next) => {
+  RefreshTokenStore.deleteByToken(req.cookies['refresh-token'], (err, tokenDeleted) => {
+    if (err) return next();
+    tokens.deleteTokenCookies(res);
+    return res.status(200).json({
+      message: 'Logout successful'
+    });
+  });
 });
 
 module.exports = router;
--- tokens.js
+++ tokens.js
@@ -8,138 +8,140 @@ var User = require('./models/user');
 var RefreshTokenStore = require('./models/refreshTokenStore');
 
 var checkTokens = function(req, res, next) {
-    var accessToken = req.cookies['access-token'];
-    var refreshToken = req.cookies['refresh-token'];
-    var csrfToken = req.headers['x-csrf-token'];
+  var accessToken = req.cookies['access-token'];
+  var refreshToken = req.cookies['refresh-token'];
+  var csrfToken = req.headers['x-csrf-token'];
 
-    console.log(accessToken);
-    console.info(refreshToken);
-    console.info(csrfToken);
-    
-    if (accessToken) {
-        //Verify the access token
-        jwt.verify(accessToken, config.jwtSecret, (err, decoded) => {
+  if (accessToken) {
+    //Verify the access token
+    jwt.verify(accessToken, config.jwtSecret, (err, decoded) => {
+      //Access token valid
+      if(err == null) {
+        if(decoded.csrf_token !== csrfToken) {
+          return res.status(401).json({
+            message: 'Invalid token'
+          });
+        }
+        req.decoded = decoded;
+        return next();
+      }
 
-            //Access token valid
-            if(err == null){
-                if(decoded.csrf_token !== csrfToken){
-                    return res.status(401).json({
-                        message: 'Invalid token'
-                    });
-                }
-                req.decoded = decoded;
-                return next();
-            }
-
-            //Access Token Expired
-            if (err.name === 'TokenExpiredError') {
-                decoded = jwt.decode(accessToken);
-                if(decoded.csrf_token !== csrfToken){
-                    return res.status(401).json({
-                        message: 'Invalid token'
-                    });
-                }
-                console.info('Token expired. Refreshing tokens.');
-                req.decoded = decoded;
-                refreshTokens(decoded, refreshToken, (err, newTokens) => {
-                    if(err){
-                        console.error('Error refresing tokens: ' + err);
-                        return res.status(401).json({
-                            message: 'Token expired. Invalid refresh token'
-                        });
-                    }
-                    setTokenCookies(res, newTokens);
-                    return next();
-                });
-            }
-
-            //Access token invalid    
-            else {
-                return res.status(401).json({
-                    message: 'Invalid token'
-                });
-            }
+      //Access Token Expired
+      if (err.name === 'TokenExpiredError') {
+        decoded = jwt.decode(accessToken);
+        if(decoded.csrf_token !== csrfToken){
+          return res.status(401).json({
+            message: 'Invalid token'
+          });
+        }
+        console.info('Token expired. Refreshing tokens.');
+        req.decoded = decoded;
+        refreshTokens(decoded, refreshToken, (err, newTokens) => {
+          if(err){
+            console.error('Error refresing tokens: ' + err);
+            return res.status(401).json({
+              message: 'Token expired. Invalid refresh token'
+            });
+          }
+          setTokenCookies(res, newTokens);
+          return next();
         });
-    } else {
+      }
+
+      //Access token invalid    
+      else {
         return res.status(401).json({
-            message: 'No token'
+          message: 'Invalid token'
         });
-    }
+      }
+    });
+  } else {
+    return res.status(401).json({
+      message: 'No token'
+    });
+  }
 };
 
-var refreshTokens = function(atPayload, rt, callback){
-    RefreshTokenStore.validate(atPayload.user_id, rt, (err, valid) => {
+var refreshTokens = function(atPayload, rt, callback) {
+  RefreshTokenStore.validate(atPayload.user_id, rt, (err, valid) => {
+    if(err){
+      return callback(err, null);
+    }
+    if(valid){
+      createNewTokens(atPayload.user_id, rt, (err, newTokens) => {
         if(err){
-            return callback(err, null);
+          return callback(err, null);
         }
-        if(valid){
-            createNewTokens(atPayload.user_id, rt, (err, newTokens) => {
-                if(err){
-                    return callback(err, null);
-                }
-                return callback(null, newTokens);
-            });
-        } else {
-            let error = new Error('Refresh token invalid');
-            return callback(error, null);
-        }
-    });
+        return callback(null, newTokens);
+      });
+    } else {
+      let error = new Error('Refresh token invalid');
+      return callback(error, null);
+    }
+  });
 };
 
 var createNewTokens = function(userId, oldRt, callback){
-    let csrfToken = randToken.generate(16);
+  let csrfToken = randToken.generate(16);
 
-    let accessToken = jwt.sign({
-        user_id: userId,
-        csrf_token: csrfToken
-    }, config.jwtSecret,
-    {
-        expiresIn: '15m' //testing 5s, real 15m.
+  let accessToken = jwt.sign({
+    user_id: userId,
+    csrf_token: csrfToken
+  }, config.jwtSecret,
+  {
+    expiresIn: '15m' //testing 5s, real 15m.
+  });
+
+  let tokens = {
+    accessToken: accessToken,
+    refreshToken: oldRt,
+    csrfToken: csrfToken
+  };
+
+  var generateNewRt = function() {
+    console.info('New token created');
+    tokens.refreshToken = randToken.generate(16);
+    RefreshTokenStore.create(userId, tokens.refreshToken, (err) => {
+      if(err) return callback(err, null);
+      return callback(null, tokens);
     });
+  }
 
-    let tokens= {
-        accessToken: accessToken,
-        refreshToken: oldRt,
-        csrfToken: csrfToken
-    };
-
-    var generateNewRt = function(){
-        console.info('New token created');
-        tokens.refreshToken = randToken.generate(16);
-        RefreshTokenStore.create(userId, tokens.refreshToken, (err) => {
-            if(err) return callback(err, null);
-            return callback(null, tokens);
-        });
-    }
-
-    //If an old refresh token was passed, delete it from database first.
-    if(oldRt){
-        RefreshTokenStore.deleteByToken(oldRt, (err, tokenDeleted) => {
-            if(err) return callback(err, null);
-            //If no token was deleted, it was already deleted by a parallel request and a new tokens have already been created.
-            if(!tokenDeleted){
-                console.info('Tokens had already been created');
-                tokens.accessToken = null;
-                tokens.refreshToken = null;
-                tokens.csrfToken = null;
-                return callback(null, tokens);
-            } else {
-                generateNewRt();
-            }
-        });
-    } else {
+  //If an old refresh token was passed, delete it from database first.
+  if(oldRt){
+    RefreshTokenStore.deleteByToken(oldRt, (err, tokenDeleted) => {
+      if(err) return callback(err, null);
+      //If no token was deleted, it was already deleted by a parallel request and a new tokens have already been created.
+      if(!tokenDeleted){
+        console.info('Tokens had already been created');
+        tokens.accessToken = null;
+        tokens.refreshToken = null;
+        tokens.csrfToken = null;
+        return callback(null, tokens);
+      } else {
         generateNewRt();
-    }
+      }
+    });
+  } else {
+    generateNewRt();
+  }
 }
 
-var setTokenCookies = function(res, tokens){
-    if(tokens.accessToken) res.cookie('access-token', tokens.accessToken, {httpOnly: true/*, secure: true*/});
-    if(tokens.refreshToken) res.cookie('refresh-token', tokens.refreshToken, {httpOnly: true/*, secure: true*/});
-    if(tokens.csrfToken) res.cookie('csrf-token', tokens.csrfToken/*, {secure: true}*/);
+var setTokenCookies = function(res, tokens) {
+  if(tokens.accessToken) res.cookie('access-token', tokens.accessToken, {httpOnly: true/*, secure: true*/});
+  if(tokens.refreshToken) res.cookie('refresh-token', tokens.refreshToken, {httpOnly: true/*, secure: true*/});
+  if(tokens.csrfToken) res.cookie('csrf-token', tokens.csrfToken/*, {secure: true}*/);
+}
+
+var deleteTokenCookies = function(res) {
+  res.clearCookie('access-token');
+  res.clearCookie('refresh-token');
+  res.clearCookie('csrf-token');
 }
 
 module.exports = {
-    checkTokens: checkTokens,
-    createNewTokens: createNewTokens,
-    setTokenCookies: setTokenCookies
+  checkTokens: checkTokens,
+  createNewTokens: createNewTokens,
+  setTokenCookies: setTokenCookies,
+  deleteTokenCookies: deleteTokenCookies
 }
