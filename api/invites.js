5c0e1e39192596edf0011db683a65dbebb674b9e
Edgardo Cuello <ecuellog@gmail.com>
2020-08-19 17:09:34 -0700

    Update ledger friend info with user info when a user accepts an invite

--- api/invites.js
+++ api/invites.js
@@ -4,6 +4,7 @@ var tokens = require('../tokens');
 var Ledger = require('../models/ledger');
 var Invite = require('../models/invite');
 var Friend = require('../models/friend');
+var User = require('../models/user');
 var mongoose = require('mongoose');
 
 // Accept an invite
@@ -15,8 +16,12 @@ router.get('/:inviteId/accept', tokens.checkTokens, async (req, res) => {
     let invite = await Invite.findById(req.params.inviteId);
     let friend = await Friend.findById(invite.friendId);
     let ledger = await Ledger.findById(invite.ledgerId);
+    let user = await User.findById(req.decoded.user_id);
 
     friend.userId = req.decoded.user_id;
+    friend.email = user.email;
+    friend.avatarSrc = user.avatarSrc;
+    friend.avatarColor = user.avatarColor;
     ledger.sharedWith = [...ledger.sharedWith, req.decoded.user_id];
 
     await friend.save({ session });
